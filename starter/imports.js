e8dcde538a8772023cc6fd343ea95a88a93dfee6
mgusmano <mgusmano@yahoo.com>
2026-04-29 08:12:45 -0400

    all

---
+++ starter/imports.js
@@ -0,0 +1,4 @@
+// import _find from "lodash.find";
+// import SignaturePad from "signature_pad";
+// Ext._find = _find;
+// Ext.SignaturePad = SignaturePad;
---
+++ starter/vite.config.mjs
@@ -0,0 +1,25 @@
+import path from "path";
+import fs from "node:fs";
+import { defineConfig } from "vite";
+import { ExtKitPlugin } from "./node_modules/@sencha/extkit/ExtKitPlugin.js";
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [ExtKitPlugin()],
+    server: {
+      port: 2080,
+      hmr: true,
+      //hmr: { protocol: "ws", host: "localhost" },
+    },
+    build: {
+      minify: true,
+      sourcemap: true,
+      outDir: "./vite",
+      lib: {
+        entry: path.resolve(__dirname, "imports.js"),
+        fileName: (format) => `imports.${format}.js`,
+        formats: ["es"],
+      },
+    },
+  };
+});
