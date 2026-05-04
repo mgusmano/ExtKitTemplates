5c984fee1dcb276188e7cffd5a86de6c80e256b6
mgusmano <mgusmano@yahoo.com>
2026-05-04 06:28:09 -0400

    new

---
+++ modern/common/imports.js
@@ -0,0 +1,2 @@
+import _find from "lodash.find";
+Ext._find = _find;
--- starter/imports.js
+++
@@ -1,4 +0,0 @@
-// import _find from "lodash.find";
-// import SignaturePad from "signature_pad";
-// Ext._find = _find;
-// Ext.SignaturePad = SignaturePad;
